b72935e7282b2161f6d22031279e26d51875a6bb
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-17 12:26:32 -0700

    Added drawer featyre

--- try_side_menu_nativeBase/App.js
+++ try_side_menu_nativeBase/App.js
@@ -1,57 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry,
+  Text
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Drawer} from 'native-base';
 
-export default class App extends Component<{}> {
+import AppHeader from './AppHeader';
+
+import Sidebar from './Sidebar';
+
+export default class App extends Component {
+  closeDrawer = () => {
+    this.drawer._root.close()
+  };
+  openDrawer = () => {
+    this.drawer._root.open()
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Drawer
+        ref={(ref) => { this.drawer = ref; }}
+        content={<Sidebar/>}
+        onClose={() => this.closeDrawer()} >
+
+        <AppHeader
+            openDrawer={this.openDrawer.bind(this)}
+        />
+        </Drawer>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+module.exports = App;
