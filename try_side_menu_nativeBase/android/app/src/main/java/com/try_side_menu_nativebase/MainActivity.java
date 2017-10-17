50f2cfec44c5af2d32ee76272902693f7a5de03b
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-17 11:59:05 -0700

    init new project + installed nativeBase

---
+++ try_side_menu_nativeBase/android/app/src/main/java/com/try_side_menu_nativebase/MainActivity.java
@@ -0,0 +1,15 @@
+package com.try_side_menu_nativebase;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "try_side_menu_nativeBase";
+    }
+}
---
+++ try_side_menu_nativeBase/index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('try_side_menu_nativeBase', () => App);
