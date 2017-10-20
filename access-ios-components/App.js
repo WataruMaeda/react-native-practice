1ad71da25e5c0c256978874877094a65d6de0cc0
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-20 12:38:30 -0700

    sets up environments

--- access-ios-components/App.js
+++ access-ios-components/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      log: 'Log'
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.log}</Text>
       </View>
     );
   }
