7e38c3ffecdbebb0905ae3b51abf136a451185f3
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-23 16:00:17 -0700

    Added nomal list view

--- try-audio/App.js
+++ try-audio/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+class Item {
+  constructor(index, title, subtitle) {
+    this.index = index;
+    this.title = title;
+    this.subtitle = subtitle;
+  }
+}
+
+const items = [
+  new Item (
+    0,
+    'title-1',
+    'subtitle-1'
+  ),
+  new Item (
+    1,
+    'title-2',
+    'subtitle-2'
+  ),
+  new Item (
+    2,
+    'title-3',
+    'subtitle-3'
+  )
+]
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <FlatList
+        data={items}
+        keyExtractor={item => item.index}
+        renderItem={({item})=>(<Text>{item.title}</Text>)}
+        />
       </View>
     );
   }
