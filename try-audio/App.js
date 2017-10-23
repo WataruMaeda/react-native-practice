02f1b99b0faae757f45ad9453d1e25db38c24f71
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-23 15:48:19 -0700

    init project

--- try-audio/App.js
+++ try-audio/App.js
@@ -1,49 +1,11 @@
 import React from 'react';
-import { FlatList ,StyleSheet, Text, View } from 'react-native';
-
-class Item {
-  constructor(name, imagePath, soundPath) {
-    this.name = name;
-    this.imagePath = imagePath;
-    this.soundPath = soundPath;
-  }
-}
-
-const items = [
-  new Item(
-    "Book",
-    require(".assets/images/book.jpg"),
-    require(".assets/sounds/book.mp3"),
-  ),
-  new Item(
-    "Cat",
-    require(".assets/images/cat.jpg"),
-    require(".assets/sounds/cat.mp3"),
-  ),
-  new Item(
-    "Farewell",
-    require(".assets/images/farewell.jpg"),
-    require(".assets/sounds/farewell.mp3"),
-  ),
-  new Item(
-    "Feel",
-    require(".assets/images/feel.jpg"),
-    require(".assets/sounds/feel.mp3"),
-  ),
-  new Item(
-    "Horror",
-    require(".assets/images/horror.jpg"),
-    require(".assets/sounds/horror.mp3"),
-  )
-]
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList>
-
-        </FlatList>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
