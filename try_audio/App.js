451bafb2b194fcc996f721d7166d2ddaec805540
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-24 22:23:31 -0700

    Added navigation

--- try_audio/App.js
+++ try_audio/App.js
@@ -4,103 +4,12 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Dimensions,
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  FlatList,
-  TouchableOpacity
-} from 'react-native';
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import AudioListScreen from './AudioListScreen';
 
-class Item {
-  constructor(title, image, sound) {
-    this.title = title;
-    this.image = image;
-    this.sound = sound;
-  }
-}
-
-const items = [
-  new Item (
-    'March',
-    require('./assets/images/march.jpg'),
-    require('./assets/sounds/march.mp3')
-  ),
-  new Item (
-    'Mist',
-    require('./assets/images/mist.jpg'),
-    require('./assets/sounds/mist.mp3')
-  ),
-  new Item (
-    'Piano Man',
-    require('./assets/images/pianoman.jpg'),
-    require('./assets/sounds/pianoman.mp3')
-  ),
-  new Item (
-    'Shopping',
-    require('./assets/images/shop.jpg'),
-    require('./assets/sounds/shop.mp3')
-  ),
-  new Item (
-    'Starts',
-    require('./assets/images/star.jpg'),
-    require('./assets/sounds/star.mp3')
-  )
-]
-
-const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
-
-export default class App extends Component {
-
-  _keyExtractor = (item, index) => item.title;
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-        data={items}
-        keyExtractor={this._keyExtractor}
-        renderItem={this._renderRow}
-        />
-      </View>
-    );
-  }
-
-  _renderRow = ({item}) => (
-    <TouchableOpacity onPress={() => this.tappedRow({item})}>
-      <ImageBackground
-      source={item.image}
-      style={{width: DEVICE_WIDTH, height: 150}}>
-        <Text style={styles.cell_text}>{item.title}</Text>
-      </ImageBackground>
-    </TouchableOpacity>
-  );
-
-  tappedRow({item}) {
-    console.log(item.title);
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  cell_text: {
-    height: '100%',
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'white',
-    fontStyle: 'italic',
-    fontFamily: 'Baskerville',
-    backgroundColor: 'transparent',
-    textAlign: 'center'
-  }
+const App = StackNavigator({
+  AudioList: { screen: AudioListScreen }
 });
+
+export default App;
