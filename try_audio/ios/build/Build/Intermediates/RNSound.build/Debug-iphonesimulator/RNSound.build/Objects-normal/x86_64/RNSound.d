93e977f98f44a061906a582e199ba734e06145da
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-10-27 22:33:05 -0700

    install sound library

---
+++ try_audio/ios/build/Build/Intermediates/RNSound.build/Debug-iphonesimulator/RNSound.build/Objects-normal/x86_64/RNSound.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/wmaeda/Documents/GitHub/react-native-practice/try_audio/node_modules/react-native-sound/RNSound/RNSound.m \
+  /Users/wmaeda/Documents/GitHub/react-native-practice/try_audio/node_modules/react-native-sound/RNSound/RNSound.h \
+  ../react-native/React/Base/RCTBridgeModule.h \
+  /Users/wmaeda/Documents/GitHub/react-native-practice/try_audio/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  ../react-native/React/Base/RCTUtils.h \
+  /Users/wmaeda/Documents/GitHub/react-native-practice/try_audio/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h
