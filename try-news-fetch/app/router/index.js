af4e2f379b8f5cc635569a3dbbd2d63d3772148c
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-02-15 12:24:19 -0800

    style naviagtion

--- try-news-fetch/app/router/index.js
+++ try-news-fetch/app/router/index.js
@@ -1,5 +1,14 @@
 import { StackNavigator } from 'react-navigation'
 import Home from '../screens/home'
 export default StackNavigator({
-    Home: { screen: Home },
+    Home: { 
+        screen: Home,
+        navigationOptions: ({ navigation }) => ({
+            title: `News Feeds`,
+            headerTintColor: "#fff",
+            headerStyle: { backgroundColor: '#34495e', },
+            headerTitleStyle: { fontSize: 18, },
+          }),
+  
+    },
 })
