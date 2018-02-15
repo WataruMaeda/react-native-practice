1579f37c124a0418b207c5ca4cccc5b4182392f1
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-02-15 13:05:43 -0800

    integrated redux to the home screen

--- try-news-fetch/app/screens/home/Home.js
+++ try-news-fetch/app/screens/home/Home.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styles from './styles'
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+// redux
+import { connect } from 'react-redux'
+import { updateFeeds } from '../../redux/actions/feedManager'
+
+class Home extends React.Component {
+    render() {
+        const { feeds } = this.props
+        return (
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        );
+      }
 }
+
+const mapStateToProps = state => {
+    return {
+        feeds: state.FeedManager.feeds,
+    }
+}
+
+export default connect(mapStateToProps)(Home)
